26af626b284e2f8aff24140871b9ba5d9ada05c8
Colin Brust <colin.brust@gmail.com>
2024-06-21 11:39:47 -0600

    Use census shp instead of raster.

--- drought_stats/R/county_in_drought.R
+++ drought_stats/R/county_in_drought.R
@@ -1,4 +1,8 @@
-pop <- terra::rast("./drought_stats/data/pop_raster.tif")
+library(magrittr)
+
+pop <- sf::read_sf("~/data/census/tl_2020_30_tabblock20.shp") %>% 
+  dplyr::select(POP20)
+
 drought <- sf::read_sf("/vsizip//vsicurl/https://droughtmonitor.unl.edu/data/shapefiles_m/USDM_current_M.zip") %>% 
   sf::st_make_valid() %>% 
   dplyr::transmute(usdm_class = factor(paste0("D", DM),
@@ -10,20 +14,14 @@ drought <- sf::read_sf("/vsizip//vsicurl/https://droughtmonitor.unl.edu/data/sha
   sf::st_cast("MULTIPOLYGON") %>%
   dplyr::arrange(usdm_class) %>% 
   sf::st_transform(sf::st_crs(pop)) %>% 
-  sf::st_crop(pop) %>% 
-  terra::rasterize(pop, field = "usdm_class")
+  sf::st_crop(pop) 
 
-
-raster_to_points <- list(pop, drought) %>% 
-  terra::rast() %>% 
-  terra::as.data.frame(xy=TRUE) %>% 
-  sf::st_as_sf(coords = c("x", "y")) %>% 
-  sf::`st_crs<-`(sf::st_crs(pop)) 
+joined <- sf::st_join(pop, drought, join = sf::st_intersects)
 
 counties <- sf::read_sf("https://data.climate.umt.edu/mca/fgb/mt_counties.fgb") %>% 
   sf::st_transform(sf::st_crs(pop))
 
-sf::st_join(raster_to_points, counties, join = sf::st_intersects) %>% 
-  dplyr::filter(!is.na(County), !is.na(focal_mean), !is.na(usdm_class)) %>% 
+summary <- sf::st_join(joined, counties, join = sf::st_intersects) %>% 
+  dplyr::filter(!is.na(County), !is.na(POP20), !is.na(usdm_class)) %>% 
   dplyr::group_by(usdm_class, County) %>% 
-  dplyr::summarise(pop_in_drought = sum(focal_mean, na.rm = T))
+  dplyr::summarise(pop_in_drought = sum(POP20, na.rm = T))
