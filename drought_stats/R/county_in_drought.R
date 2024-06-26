d9143779ba279d2b7a6681f0991c9e93365e9527
colinbrust <colin.brust@gmail.com>
2024-06-26 15:52:33 -0600

    Working on county drought analysis.

--- drought_stats/R/county_in_drought.R
+++ drought_stats/R/county_in_drought.R
@@ -1,13 +1,17 @@
 library(magrittr)
 
-pop <- sf::read_sf("~/data/census/tl_2020_30_tabblock20.shp") %>% 
+pop <- sf::read_sf("~/data/census/tl_2020_30_tabblock20/tl_2020_30_tabblock20.shp") %>% 
   dplyr::select(POP20)
+
+county_codes <- readr::read_delim(
+  "https://www2.census.gov/geo/docs/reference/codes2020/cou/st30_mt_cou2020.txt", 
+  delim = "|"
+) %>% 
+  dplyr::select(COUNTYFP20=COUNTYFP, county=COUNTYNAME)
 
 drought <- sf::read_sf("/vsizip//vsicurl/https://droughtmonitor.unl.edu/data/shapefiles_m/USDM_current_M.zip") %>% 
   sf::st_make_valid() %>% 
-  dplyr::transmute(usdm_class = factor(paste0("D", DM),
-                                       levels = paste0("D", 0:4),
-                                       ordered = TRUE)) %>%
+  dplyr::transmute(usdm_class = paste0("D", DM)) %>%
   # Group by date and class, and generate multipolygons
   dplyr::group_by(usdm_class) %>%
   dplyr::summarise(.groups = "keep") %>%
@@ -18,10 +22,41 @@ drought <- sf::read_sf("/vsizip//vsicurl/https://droughtmonitor.unl.edu/data/sha
 
 joined <- sf::st_join(pop, drought, join = sf::st_intersects)
 
-counties <- sf::read_sf("https://data.climate.umt.edu/mca/fgb/mt_counties.fgb") %>% 
-  sf::st_transform(sf::st_crs(pop))
+pop_in_drought <- joined %>% 
+  dplyr::left_join(county_codes) %>% 
+  dplyr::select(county, population=POP20, usdm_class) %>% 
+  sf::st_drop_geometry() %>% 
+  dplyr::group_by(county, usdm_class) %>% 
+  dplyr::summarise(pop_in_drought = sum(population)) %>% 
+  dplyr::mutate(usdm_class = ifelse(is.na(usdm_class), 'No Drought', usdm_class)) %>%
+  dplyr::mutate(usdm_class = factor(
+    usdm_class,
+    levels = c("No Drought", "D0", "D1", "D2", "D3", "D4"),
+    ordered = TRUE
+  )) %>%
+  dplyr::mutate(
+    County = stringr::str_replace(county, " County", "")
+  ) %>% 
+  dplyr::ungroup() %>% 
+  dplyr::select(-county) %>%
+  dplyr::group_by(County) %>% 
+  tidyr::complete(usdm_class = factor(levels(usdm_class), levels = levels(usdm_class), ordered = TRUE)) %>%
+  dplyr::mutate(
+    pop_in_drought = ifelse(is.na(pop_in_drought), 0, pop_in_drought)
+  )
 
-summary <- sf::st_join(joined, counties, join = sf::st_intersects) %>% 
-  dplyr::filter(!is.na(County), !is.na(POP20), !is.na(usdm_class)) %>% 
-  dplyr::group_by(usdm_class, County) %>% 
-  dplyr::summarise(pop_in_drought = sum(POP20, na.rm = T))
+summary <- sf::read_sf("https://data.climate.umt.edu/mca/fgb/mt_counties.fgb") %>% 
+  sf::st_transform(sf::st_crs(pop)) %>% 
+  dplyr::left_join(pop_in_drought, by = "County") 
+
+map <- mapview::mapview()
+
+for (d in c("No Drought", "D0", "D1", "D2", "D3", "D4")) {
+  summary %>% 
+    dplyr::filter(usdm_class == d) %>% 
+    dplyr::select(-County) %>% 
+    mapview::mapview(map = map, zcol = pop_in_drought)
+}
+
+map
+sf::write_sf(summary, "./drought_stats/data/latest_pid.fgb")
