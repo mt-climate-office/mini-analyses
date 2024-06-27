10aa2c8ebaf73f2c7779d8ee7536a8466d69054e
colinbrust <colin.brust@gmail.com>
2024-06-27 12:16:05 -0600

    Script to get all current climate at a glance stats.

--- drought_stats/R/climate_stats.R
+++ drought_stats/R/climate_stats.R
@@ -4,14 +4,86 @@ f = "https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time
 get_climate_ts <- function(
   county_num = "MT-001",
   variable = "tavg",
-  start_year = 1985,
+  start_year = 1895,
   end_year = lubridate::today() |> lubridate::year(),
-  timescale = "1",
+  timescale = 1,
   month = (lubridate::today() - lubridate::dmonths(1)) |> lubridate::month(),
   por_start_year = 1991,
-  por_end_year = 2020
+  por_end_year = 2020,
+  rank_desc = FALSE,
+  ...
 ) {
+  print(county_num)
+  print(variable)
+  response <- file.path(
+    "https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/time-series",
+    county_num, 
+    variable,
+    timescale,
+    month,
+    glue::glue("{start_year}-{end_year}.csv")
+  ) |>
+    httr::GET(
+      query = list(
+        base_prd = TRUE,
+        begbaseyear = por_start_year,
+        endbaseyear = por_end_year
+      )
+    )
   
+  meta <- httr::content(response, n_max = 3, show_col_types = FALSE)
+  
+  na_val <- meta[ifelse(variable %in% c("pdsi", "phdi", "pmdi", "zndx"), 2, 3),1] |> 
+    as.character() |> 
+    stringr::str_replace("Missing: ", "") |> 
+    as.numeric()
+  
+  httr::content(
+    response, 
+    skip = ifelse(variable %in% c("pdsi", "phdi", "pmdi", "zndx"), 3, 4), 
+    show_col_types = FALSE
+  ) |>
+    janitor::clean_names() |>
+    dplyr::mutate(
+      value = dplyr::na_if(value, na_val),
+      anomaly = dplyr::na_if(anomaly, na_val),
+      date = as.character(date) |>
+        paste0("01") |>
+        lubridate::as_date(),
+      rank_asc = dplyr::dense_rank(value),
+      ranc_desc = dplyr::dense_rank(dplyr::desc(value))
+    )
 }
 
-readr::read_csv(f, skip=4)
+
+get_all_county_data <- function() {
+  
+  variables <- tibble::tribble(
+    ~code, ~name,
+    "cdd", "Cooling Degree Days",
+    "hdd", "Heating Degree Days",
+    "tavg", "Average Temperature",
+    "tmax", "Maximum Temperature",
+    "tmin", "Minimum Temperature",
+    "pcp", "Precipitation",
+    "pdsi", "Palmer Drought Severity Index",
+    "phdi", "Palmer Hydrological Drought Index",
+    "pmdi", "Palmer Modified Drought Index",
+    "zndx", "Palmer Z-Index"
+  )
+  
+  dat <- tidycensus::fips_codes |>
+    tibble::as_tibble() |>
+    dplyr::filter(
+      state == "MT"
+    ) |>
+    dplyr::mutate(
+      cnty_id = glue::glue("{state}-{county_code}")
+    ) |>
+    dplyr::select(county, cnty_id) |>
+    tidyr::crossing(variables) |>
+    dplyr::rowwise() |>
+    dplyr::mutate(dat = list(get_climate_ts(county_num = cnty_id, variable = code)))
+}
+# NOAA National Centers for Environmental information, Climate at a Glance: County Rankings, published June 2024, 
+# retrieved on June 27, 2024 from https://www.ncei.noaa.gov/access/monitoring/climate-at-a-glance/county/rankings 
